c2ef14c4c484148d89f7f1267aa1d4ff6a4f2887
Rachael Kondrat <rkondrat@S0405.local>
2016-11-06 09:59:35 -0500

    initial commit

---
+++ Assets/moveme.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveme : MonoBehaviour {
+
+	public float speed = 5.0f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool getLeft = Input.GetKey (KeyCode.A);
+		bool getRight = Input.GetKey (KeyCode.D);
+
+		Vector3 currentPosition = transform.position; 
+		if (getLeft)
+		{
+			
+			currentPosition.x -= speed * Time.deltaTime;
+		} else if(getRight) { 
+			
+			currentPosition.x += speed * Time.deltaTime;
+		} 
+			transform.position = currentPosition; 
+	}
+}
+
---
+++ Assets/randonins.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randonins : MonoBehaviour {
+
+	public float secondsSpawn = 5.0f; 
+	public GameObject fallingCube; 
+
+	//private variables 
+
+	private Vector3 higherUpPosition; 
+	private Vector3 offset; 
+	private bool fallingSpawned = false; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!fallingSpawned) { 
+			StartCorountine (SpawnedFallingCubeEveryXSeconds (secondsSpawned));
+	
+		}
+	}
+
+		IEnumerator SpawnedFallingCubeEveryXSeconds (float seconds)
+	{
+		fallingSpawned = true;
+		offset = new Vector3 (Random.Range (-20.0f, 20.0f), 50.0f, Random.Range (-20.0f, 20.0f));
+		higherUpPosition = Vector3.up * Random.Range (20.0f, 150.0f) + offset; 
+		Instantiate (fallingCube, higherUpPosition, Quaternion.identity); 
+		yield return new WaitForSeconds (seconds); 
+		fallingSpawned = false; 
+
+	}
+}
